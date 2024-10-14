c90111fd813bc6a53e35545d3737035a8073a898
XAELLA12 <ripamontigabriella3@gmail.com>
2024-10-14 17:34:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let currentSlide = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Cambia slide ogni 3 secondi
+setInterval(() => {
+    showSlide(currentSlide + 1);
+}, 3000);
 
